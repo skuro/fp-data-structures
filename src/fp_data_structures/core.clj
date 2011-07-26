a2633fa5473cbcb23a67489eb8125694786149f7
Carlo Sciolla <skuro@shardana.(none)>
2011-07-26 23:33:37 +0200

    initial import

---
+++ src/fp_data_structures/core.clj
@@ -0,0 +1,1 @@
+(ns fp-data-structures.core)
---
+++ test/fp_data_structures/test/core.clj
@@ -0,0 +1,6 @@
+(ns fp-data-structures.test.core
+  (:use [fp-data-structures.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
