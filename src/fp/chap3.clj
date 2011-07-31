d3a88761331caae72f9f9cd78b95996b50d1215d
Carlo Sciolla <skuro@skuro.tk>
2011-07-31 23:45:41 +0200

    use make-queue

--- src/fp/chap3.clj
+++ src/fp/chap3.clj
@@ -4,10 +4,16 @@
 ;; in this implementation the queue holds values
 ;; in two different buckets, front and rear.
 ;; front is empty only if rear is empty.
-(defn create-queue
-  []
-  (atom {:f '()
-         :r '()}))
+(defn make-queue
+  [f r]
+  (atom {:f f
+         :r r}))
+
+(defn check-invariant
+  [q]
+  (if (empty? (:f @q))
+    (swap! q #(make-queue (reverse (:r @q))
+                          '()))))
 
 ;; returns the first element in the queue
 (defn head
@@ -18,15 +24,13 @@
 ;; the invariant it adds to front in case it's empty
 (defn snoc
   [q x]
-  (let [k (if (empty? (:f @q)) :f :r)]
-    (swap! q #(assoc % k (conj (% k) x)))))
+  (check-invariant (make-queue (:f @q)
+                               (conj (:r @q) x))))
 
 ;; normally takes out the head of front, but to keep
 ;; the invariant it reverses rear and moves it to front
 ;; in case front has only one value to pop
 (defn tail
   [q]
-  (let [res (head q)]
-    (if (= 1 (count (:f @q)))
-      (swap! q #(let [new-f (reverse (:r %))] {:f new-f :r '()})))
-    res))
+  (check-invariant (make-queue (rest (:f @q))
+                               (:r @q))))
